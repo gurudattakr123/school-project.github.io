9b3d9951cae6421a369cd47b88df61f942037d87
unknown <gurudattakr123@gmail.com>
2018-10-17 18:48:30 +0530

    message

--- models/Teachers.js
+++ models/Teachers.js
@@ -14,10 +14,17 @@ const TeacherSchema = mongoose.Schema({
     class_id : String,
     doj : String, 
     subjects : Array,
+    attendance : [{refs: 'Teacherattendance'}],
     username: String,
     password : String
 });
 
+
+const Teacherattendance = mongoose.Schema({
+    teacher_id : {refs: 'TeacherSchema' },
+    date:Date,
+    status: String
+})
 
 TeacherSchema.methods.hashPassword = function (password) {
     return bcrypt.hashSync(password, 10);
@@ -29,5 +36,5 @@ TeacherSchema.methods.hashPassword = function (password) {
 { versionKey: false }
 
 
-
+module.exports = mongoose.model('Teacherattendance', Teacherattendance, 'Teacherattendance')
 module.exports = mongoose.model('teacher', TeacherSchema, 'teacher');
