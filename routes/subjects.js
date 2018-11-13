d83acbec2d403d7140bd3fd2332ee126295a4432
unknown <gurudattakr123@gmail.com>
2018-11-13 15:01:37 +0530

    deleted select2

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const classes = require('../models/Classes')
 router.get('/list', isValidUser, function(req, res, next){
-    res.render('subjects-list');
+    classes.find({}, {class_name:1, class_id:1}, function(err, cls_names){
+        res.render('subjects-list',{classes: cls_names});
+    })
 })
 
 router.get('/add', isValidUser, function(req, res, next){
     res.render('add-subject');
 })
 
+router.post('/update_subjects', function(req, res, next){
+    console.log(req.body)
+})
 
 function isValidUser(req,res,next){
     if(req.isAuthenticated()){
